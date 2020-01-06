3711d2449762515bcf36a039d4e0bf3c85ec07ac
Elena Stupina <stupina.work@gmail.com>
2020-01-06 22:40:47 +0300

    Add validation schemas

---
+++ api/api/schemas.py
@@ -0,0 +1,77 @@
+from marshmallow import fields, Schema, validate
+
+
+class BookSchema(Schema):
+    id = fields.Int()
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(max=60),
+    )
+
+
+class BookInputSchema(Schema):
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(max=60),
+    )
+    authors = fields.List(
+        fields.Str(),
+        data_key='author',
+        required=True,
+    )
+
+
+class BookOutputSchema(Schema):
+    id = fields.Int()
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(max=60),
+    )
+    authors = fields.Nested(
+        "AuthorSchema",
+        many=True,
+        required=True,
+    )
+
+
+BookSchema = BookSchema()
+BookInputSchema = BookInputSchema()
+BookOutputSchema = BookOutputSchema(many=True)
+
+
+class AuthorSchema(Schema):
+    id = fields.Int()
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(max=60),
+    )
+
+
+class AuthorInputSchema(Schema):
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(max=60),
+    )
+    books = fields.List(
+        fields.Str(),
+        data_key='book',
+        required=True,
+    )
+
+
+class AuthorOutputSchema(Schema):
+    id = fields.Int()
+    name = fields.Str(
+        required=True,
+        validate=validate.Length(max=60),
+    )
+    books = fields.Nested(
+        "BookSchema",
+        many=True,
+        required=True,
+    )
+
+
+AuthorSchema = AuthorSchema()
+AuthorInputSchema = AuthorInputSchema()
+AuthorOutputSchema = AuthorOutputSchema(many=True)
